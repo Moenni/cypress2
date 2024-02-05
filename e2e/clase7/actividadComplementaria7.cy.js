984549fcde0594822f2d1dbe63dcc54032729367
moenni <nicolasmoen@live.com>
2024-02-05 00:46:17 -0300

    termino actividad 7

--- e2e/clase7/actividadComplementaria7.cy.js
+++ e2e/clase7/actividadComplementaria7.cy.js
@@ -3,19 +3,38 @@ describe('Actividad complementaria 7', () => {
     beforeEach("Actividad complementaria 7", () => {
        cy.visit('');
        cy.get("#registertoggle").dblclick()
-       cy.get('#user').type('pushingit')
+       cy.get('#user').type('pushingit1')
        cy.get('#pass').type('123456!')
        cy.get('#submitForm').click()
-       cy.xpath(`//h2[starts-with(@id,'user_pushingit')]`).should('exist')
+       cy.xpath(`//h2[starts-with(@id,'user_pushingit1')]`).should('exist')
        cy.contains('To Do List').click()
    })
 
      it("Deberia sacar una foto completa", () =>{
        //agregar 3 tareas y sacar una foto a la pantalla completa
+       cy.get('[data-cy="removeAll"]',{timeout:60000}).should('be.visible').click()
+       cy.get('#task').wait(5000).type('Cocinar')
+       cy.get('#sendTask').click()
+       cy.get('#task').wait(5000).type('Planchar')
+       cy.get('#sendTask').click()
+      cy.get('#task').wait(5000).type('Estudiar')
+       cy.get('#sendTask').click()
+       cy.wait(10000)
+       cy.screenshot()
    })
  
    it("Deberia sacar una foto a las tres tareas", () =>{
        //agregar 3 tareas y sacar una foto a las tareas (las 3 tareas en la misma foto - cuadro celeste)
+       cy.get('[data-cy="removeAll"]',{timeout:60000}).should('be.visible').click()
+       cy.get('#task').wait(2000).type('Cocinar')
+       cy.get('#sendTask').click()
+       cy.get('#task').wait(2000).type('Planchar')
+       cy.get('#sendTask').click()
+      cy.get('#task').wait(2000).type('Estudiar')
+       cy.get('#sendTask').click()
+       cy.wait(5000)
+       cy.get('.css-ha1fhc').screenshot() 
+       
    })
 })
 
